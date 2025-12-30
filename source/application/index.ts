98a42616b48773b3a1ccce26925a45f196e2aa59
Felipe Juris Jacques <sr.jj.arrebenta@protonmail.com>
2025-12-30 18:34:39 -0300

    first commit

---
+++ source/application/index.ts
@@ -0,0 +1,58 @@
+let video = ''
+const parameters = new URLSearchParams(window.location.search)
+const type = parameters.get('e')
+// switch (type) {
+//     case 'ny':
+const date = new Date()
+date.setMonth(date.getMonth() + 6)
+video = './assets/videos/nw1.mp4'
+window.document.body.insertAdjacentHTML('beforeend', `
+    <h1 class="title">
+        Feliz ${date.getFullYear()}
+    </h1>
+    <p class="message">
+        Um ano novo cheio de realizações te espera!
+    </p>
+`)
+//         break
+//     default:
+//         break
+// }
+
+if (video) {
+    window.document.body.insertAdjacentHTML('beforeend', `<div class="video-container">
+        <video class="video-bg" autoplay muted loop playsinline>
+            <source src="${video}" type="video/mp4">
+        </video>
+        <video class="video-main" autoplay muted loop playsinline>
+            <source src="${video}" type="video/mp4">
+        </video>
+    </div>`)
+
+    const vBg = document.querySelector('.video-bg') as HTMLVideoElement
+    const vMain = document.querySelector('.video-main') as HTMLVideoElement
+    vMain.onplay = () => vBg.play()
+    vMain.onpause = () => vBg.pause()
+
+    setTimeout(() => {
+        if (window.navigator.share !== undefined) {
+            window.document.body.insertAdjacentHTML('beforeend', `<button class="share">
+                Celebre com seus amigos
+            </button>`)
+        }
+    }, 3000)
+}
+
+window.addEventListener('click', event => {
+    if (event.target) {
+        if (event.target instanceof HTMLButtonElement) {
+            if (event.target.className === 'share') {
+                window.navigator.share({
+                    url: `${window.location.origin}`,
+                    title: 'Celebre esse momento comigo',
+                    // url: `${window.location.origin}?e=${type}`,
+                })
+            }
+        }
+    }
+})
